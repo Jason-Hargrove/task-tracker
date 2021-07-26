33795078f2dd21ce04d23143ab4b85e16b29b758
Jason Hargrove <rijaha@gmail.com>
2021-07-26 14:23:12 -0500

    Added Tasks.js and an array to App.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Button = ({ color, text, onClick }) => {
+   
 
-
-    return <button onClick={onClick} style={{ backgroundColor: color }} 
-    className='btn'>
-        {text}
-    </button>
+    return ( 
+        <button 
+            onClick={onClick} 
+            style={{ backgroundColor: color }} 
+            className='btn'
+        >
+            {text}
+        </button>
+    )
 }
 
 Button.defaultProps = {
@@ -19,6 +21,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
