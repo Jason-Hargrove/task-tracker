70be0b0cf80b8407f0dcb390b0f4a02cabfb839f
Jason Hargrove <rijaha@gmail.com>
2021-07-27 07:18:04 -0500

    App is functional.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,8 +30,9 @@ const App = () => {
 // Add Task.
 const addTask = (task) => {
   const id = Math.floor(Math.random() * 1000) + 1
-  
-  console.log(id);
+
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
 }
 
 // Delete Task.
@@ -49,8 +52,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} 
       onDelete={deleteTask} 
       onToggle={toggleReminder} /> : 'No Tasks to Show'}
